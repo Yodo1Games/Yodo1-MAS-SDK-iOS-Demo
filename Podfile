9fb64320128af2afed9017829a1bb04ecf022c2b
ZhouYuzhen <940549652@qq.com>
2021-01-11 17:51:38 +0800

    init

---
+++ Podfile
@@ -0,0 +1,21 @@
+#use_frameworks!
+source 'https://github.com/CocoaPods/Specs.git'
+source 'https://github.com/Yodo1Games/MAS-Spec.git'
+
+platform :ios, '9.0'
+inhibit_all_warnings!
+target "Yodo1MasSdkDemo" do
+
+pod 'Masonry'
+pod 'MBProgressHUD'
+pod 'Yodo1MasSDK', '~> 0.0.0.1-beta'
+
+end
+
+target "Yodo1MasSdkDemoTests" do
+  
+end
+
+target "Yodo1MasSdkDemoUITests" do
+  
+end
--- Yodo1MasSdkDemo/ViewController.m
+++ Yodo1MasSdkDemo/ViewController.m
@@ -6,8 +6,18 @@
 //
 
 #import "ViewController.h"
+#import <Masonry/Masonry.h>
+#import <MBProgressHUD/MBProgressHUD.h>
+#import <Yodo1MasCore/Yodo1Mas.h>
+#import <AppLovinSDK/AppLovinSDK.h>
 
-@interface ViewController ()
+@interface ViewController ()<Yodo1MasRewardAdDelegate, Yodo1MasInterstitialAdDelegate, Yodo1MasBannerAdDelegate>
+
+@property (weak, nonatomic) IBOutlet UISwitch *gdprSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *coppaSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *ccpaSwitch;
+
+@property (nonatomic, strong) MBProgressHUD *hud;
 
 @end
 
@@ -15,8 +25,86 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    _hud = [[MBProgressHUD alloc] initWithView:self.view];
+    _hud.mode = MBProgressHUDModeIndeterminate;
+    _hud.label.text = @"sdk init...";
+    [_hud showAnimated:YES];
+    
+    __weak __typeof(self)weakSelf = self;
+    [[Yodo1Mas sharedInstance] initWithAppId:@"Your App Id" successful:^{
+        [weakSelf.hud hideAnimated:YES];
+        
+        weakSelf.hud.mode = MBProgressHUDModeText;
+        weakSelf.hud.label.text = @"sdk init successful";
+        [weakSelf.hud showAnimated:YES];
+        [weakSelf performSelector:@selector(hideHud) withObject:nil afterDelay:0.2];
+    } fail:^(Yodo1MasError *error) {
+        [weakSelf.hud hideAnimated:YES];
+        
+        weakSelf.hud.mode = MBProgressHUDModeText;
+        weakSelf.hud.label.text = error.localizedDescription;
+        [weakSelf.hud showAnimated:YES];
+        [weakSelf performSelector:@selector(hideHud) withObject:nil afterDelay:0.2];
+    }];
+    
+    _gdprSwitch.on = [Yodo1Mas sharedInstance].isGDPRUserConsent;
+    _coppaSwitch.on = [Yodo1Mas sharedInstance].isCOPPAAgeRestricted;
+    _ccpaSwitch.on = [Yodo1Mas sharedInstance].isCCPADoNotSell;
+    [Yodo1Mas sharedInstance].rewardAdDelegate = self;
+    [Yodo1Mas sharedInstance].interstitialAdDelegate = self;
+    [Yodo1Mas sharedInstance].bannerAdDelegate = self;
 }
 
+- (void)hideHud {
+    [_hud hideAnimated:YES];
+}
+
+- (IBAction)onRewardClicked:(UIButton *)sender {
+    [[Yodo1Mas sharedInstance] showRewardAd];
+}
+
+- (IBAction)onInterstitialClicked:(UIButton *)sender {
+    [[Yodo1Mas sharedInstance] showInterstitialAd];
+}
+
+- (IBAction)onBannerClicked:(UIButton *)sender {
+    [[Yodo1Mas sharedInstance] showBannerAd];
+}
+
+- (IBAction)onAppLovinMediationDebuggerClicked:(UIButton *)sender {
+    [[ALSdk shared] showMediationDebugger];
+}
+
+- (IBAction)onGDPRChanged:(UISwitch *)sender {
+    [Yodo1Mas sharedInstance].isGDPRUserConsent = sender.isOn;
+}
+
+- (IBAction)onCOPPAChanged:(UISwitch *)sender {
+    [Yodo1Mas sharedInstance].isCOPPAAgeRestricted = sender.isOn;
+}
+
+- (IBAction)onCCPAChanged:(UISwitch *)sender {
+    [Yodo1Mas sharedInstance].isCCPADoNotSell = sender.isOn;
+}
+
+#pragma mark - Yodo1MasAdDelegate
+- (void)onAdOpened:(Yodo1MasAdEvent *)event {
+    
+}
+
+- (void)onAdClosed:(Yodo1MasAdEvent *)event {
+    
+}
+
+- (void)onAdError:(Yodo1MasAdEvent *)event error:(Yodo1MasError *)error {
+    _hud.label.text = error.localizedDescription;
+    [_hud showAnimated:YES];
+    [self performSelector:@selector(hideHud) withObject:nil afterDelay:0.2];
+}
+
+#pragma mark - Yodo1MasRewardAdvertDelegate
+- (void)onAdRewardEarned:(Yodo1MasAdEvent *)event {
+    
+}
 
 @end
